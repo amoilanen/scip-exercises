e8ea956d44a2ea5fe02a428f6b2826377be36127
anton.al.ivanov@gmail.com <anton.al.ivanov@gmail.com>
2018-10-17 19:47:00 +0300

    Excercise 1.7

---
+++ scheme/ch1.1/1.7.scm
@@ -0,0 +1,30 @@
+(define (sqrt-iter  guess x stop-check)
+  (if (stop-check guess x)
+    guess
+    (sqrt-iter
+      (improve guess x)
+      x
+      stop-check)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (better-good-enough? guess x)
+  (<
+    (/
+      (abs
+        (- (improve guess x) guess))
+      guess)
+    0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x good-enough?))
+
+(define (better-sqrt x)
+  (sqrt-iter 1.0 x better-good-enough?))
