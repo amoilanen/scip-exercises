ca500eeda5c2ed45cfc89a5b5eca848a8a73ff64
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-10-20 23:10:34 +0300

    Use internal definitions when defining nth-root

--- scheme/ch1.1/1.8.scm
+++ scheme/ch1.1/1.8.scm
@@ -5,31 +5,25 @@
       x
       (pow x (- n 1)))))
 
-(define (nth-root-iter guess x n)
-  (if (good-enough? guess x n)
-    guess
-    (nth-root-iter
-      (improve guess x n)
-      x
-      n)))
-
-(define (improve guess x n)
-  (/
-    (+
-      (/ x (pow guess (- n 1)))
-      (* guess (- n 1)))
-    n))
-
-(define (good-enough? guess x n)
-  (<
-    (/
-      (abs
-        (- (improve guess x n) guess))
-      guess)
-    0.001))
-
 (define (nth-root x n)
-  (nth-root-iter 1.0 x n))
+  (define (good-enough? guess)
+    (<
+      (/
+        (abs
+          (- (improve guess) guess))
+        guess)
+      0.001))
+  (define (improve guess)
+    (/
+      (+
+        (/ x (pow guess (- n 1)))
+        (* guess (- n 1)))
+      n))
+  (define (nth-root-iter guess)
+    (if (good-enough? guess)
+      guess
+      (nth-root-iter (improve guess))))
+  (nth-root-iter 1.0))
 
 (define (sqrt x)
   (nth-root x 2))
