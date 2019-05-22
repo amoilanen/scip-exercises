0d9af9a212f4c7ff4179e6687c2eb1d364319cbf
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-05-23 00:41:39 +0300

    Excercise 2.58 parts a and b: modifying the constructor and accessors to work with expressions with missing parentheses

--- scheme/ch2/2.58.scm
+++ scheme/ch2/2.58.scm
@@ -1,65 +1,3 @@
-(define (variable? x) (symbol? x))
-
-(define (=number? exp num)
-  (and (number? exp) (= exp num)))
-
-(define (same-variable? v1 v2)
-  (and (variable? v1) (variable? v2) (eq? v1 v2)))
-
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else (list '+ a1 a2))))
-
-; '(a + b)
-(define (sum? x)
-  (and (pair? x) (not (null? (cddr x))) (eq? (cadr x) '+)))
-
-(define (addend s) (car s))
-
-(define (augend s) (caddr s))
-
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list '* m1 m2))))
-
-; '(a * b)
-(define (product? x)
-  (and (pair? x) (not (null? (cddr x))) (eq? (cadr x) '*)))
-
-(define (multiplier p) (car p))
-
-(define (multiplicand p) (caddr p))
-
-(define (deriv exp var)
-  (cond ((number? exp) 0)
-        ((variable? exp)
-         (if (same-variable? exp var) 1 0))
-        ((sum? exp)
-         (make-sum (deriv (addend exp) var)
-                   (deriv (augend exp) var)))
-        ((product? exp)
-         (make-sum
-           (make-product (multiplier exp)
-                         (deriv (multiplicand exp) var))
-           (make-product (deriv (multiplier exp) var)
-                         (multiplicand exp))))
-        (else
-         (error "unknown expression type -- DERIV" exp))))
-
-(newline)
-(display (deriv '(x * 3) 'x))
-
-(newline)
-(display (deriv '(x + (3 * (x + (y + 2)))) 'x))
-
-(newline)
-(display (caddr '(a + b * c + ( d + e ) * f)))
-
 (define (make-tree left right op)
   (list left right op))
 
@@ -95,35 +33,113 @@
       (cdr ops)
       (push-operator (car ops) output))))
 
+; Simplified Shunting-Yard algorithm taking advantage of Scheme expressions
+; already being lists of lists: no need to emulate recursive invocation by special handling of ( and )
 (define (build-ast infix-expr operators output)
-  (if (pair? infix-expr)
+  (if (null? infix-expr)
+    (car (push-all-operators operators output))
     (let ((current-expr (car infix-expr)))
-      (newline)
-      (display "=begin=")
-      (newline)
-      (display infix-expr)
-      (newline)
-      (display current-expr)
-      (newline)
-      (display operators)
-      (newline)
-      (display output)
-      (newline)
-      (display "=end=")
-      (cond ((is-operator? current-expr) (
+      (cond ((is-operator? current-expr)
               (if (and
                       (pair? operators)
                       (is-operator? (car operators))
                       (>= (precedence (car operators)) (precedence current-expr)))
                   (build-ast infix-expr (cdr operators) (push-operator (car operators) output))
-                  (build-ast (cdr infix-expr) (cons current-expr operators) output))))
-            ((pair? current-expr) (
+                  (build-ast (cdr infix-expr) (cons current-expr operators) output)))
+            ((pair? current-expr)
               (let ((current-output (build-ast current-expr '() '())))
-                (build-ast (cdr infix-expr) operators (cons current-output output)))))
+                (build-ast (cdr infix-expr) operators (cons current-output output))))
             (else
-              (build-ast (cdr infix-expr) operators (push-operand current-expr output)))))
-    (car (push-all-operators operators output))))
+              (build-ast (cdr infix-expr) operators (push-operand current-expr output)))))))
 
-;(display (build-ast '(a + b * c + ( d + e ) * f) '() '()))
+(define (to-infix-form ast)
+  (if (not (pair? ast))
+    ast
+    (list
+      (to-infix-form (left-of-tree ast))
+      (op-of-tree ast)
+      (to-infix-form (right-of-tree ast)))))
 
+(newline)
 (display (build-ast '(a + b + c) '() '()))
+
+(newline)
+(display (to-infix-form (build-ast '(a + b * c + ( d + e ) * f) '() '())))
+
+(newline)
+(display (to-infix-form (build-ast '(x * 3) '() '())))
+
+(define (set-parentheses expr)
+  (to-infix-form (build-ast expr '() '())))
+
+(define (variable? x) (symbol? x))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list a1 '+' a2))))
+
+; '(a + b)
+(define (sum? x)
+  (let ((x-par (set-parentheses x)))
+    (and (pair? x-par) (not (null? (cddr x-par))) (eq? (cadr x-par) '+))))
+
+(define (addend s)
+  (let ((s-par (set-parentheses s)))
+    (car s-par)))
+
+(define (augend s)
+  (let ((s-par (set-parentheses s)))
+    (caddr s-par)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+
+; '(a * b)
+(define (product? x)
+  (let ((x-par (set-parentheses x)))
+    (and (pair? x-par) (not (null? (cddr x-par))) (eq? (cadr x-par) '*))))
+
+(define (multiplier p)
+  (let ((p-par (set-parentheses p)))
+    (car p-par)))
+
+(define (multiplicand p)
+  (let ((p-par (set-parentheses p)))
+    (caddr p-par)))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+(newline)
+(display (deriv '(x * 3) 'x))
+
+(newline)
+(display (deriv '(x + (3 * (x + (y + 2)))) 'x))
+
+(newline)
+(display (deriv '(x + 3 * (x + y + 2)) 'x))
