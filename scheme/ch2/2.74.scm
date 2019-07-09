d99cd404fee41149ea58a6aeb0eb9ad8b8f0d20a
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-07-09 23:30:26 +0300

    Excercise 2.74, object oriented programming in Scheme (data-directed)

--- scheme/ch2/2.74.scm
+++ scheme/ch2/2.74.scm
@@ -2,6 +2,8 @@
 
 (define put 2d-put!)
 
+(define record-files '())
+
 (define (attach-tag type-tag contents)
   (cons type-tag contents))
 
@@ -26,9 +28,17 @@
     (cond ((null? records) #f)
       ((equal? (car (contents (car records))) name) (car records))
       (else (get-record name (cdr records)))))
+  (define (get-salary name records)
+    (let ((record (get-record name records)))
+      (if (equal? record #f)
+        #f
+        (cdr (cadr (contents record))))))
   (put 'records 'boston (tag boston-records))
   (put 'get-record 'boston (lambda (name records)
     (get-record name (contents records))))
+  (put 'get-salary 'boston (lambda (name records)
+    (get-salary name (contents records))))
+  (set! record-files (cons (tag boston-records) record-files))
 'done)
 
 (define (install-tampa-records)
@@ -40,9 +50,17 @@
     (cond ((null? records) #f)
       ((equal? (car (contents (car records))) name) (car records))
       (else (get-record name (cdr records)))))
+  (define (get-salary name records)
+    (let ((record (get-record name records)))
+      (if (equal? record #f)
+        #f
+        (cdr (cdr (contents record))))))
   (put 'records 'tampa (tag tampa-records))
   (put 'get-record 'tampa (lambda (name records)
     (get-record name (contents records))))
+  (put 'get-salary 'tampa (lambda (name records)
+    (get-salary name (contents records))))
+  (set! record-files (cons (tag tampa-records) record-files))
 'done)
 
 (define (install-seattle-records)
@@ -52,20 +70,63 @@
     (map tag (list (cons (cons 'name "megan") (cons 'compensation 2.0)) (cons (cons 'name "lauren") (cons 'compensation 1.5)) (cons (cons 'name "jake") (cons 'compensation 1.5)))))
   (define (get-record name records)
     (cond ((null? records) #f)
-      ((equal? (cadr (car (contents (car records)))) name) record)
+      ((equal? (cdr (car (contents (car records)))) name) (car records))
       (else (get-record name (cdr records)))))
+  (define (get-salary name records)
+    (let ((record (get-record name records)))
+      (if (equal? record #f)
+        #f
+        (cdr (cdr (contents record))))))
   (put 'records 'seattle (tag seattle-records))
   (put 'get-record 'seattle (lambda (name records)
     (get-record name (contents records))))
+  (put 'get-salary 'seattle (lambda (name records)
+    (get-salary name (contents records))))
+  (put 'record-files 'seattle (lambda (name records)
+    (get-salary name (contents records))))
+  (set! record-files (cons (tag seattle-records) record-files))
 'done)
+
+(define (get-record name records)
+  (contents ((get 'get-record (type-tag records)) name records)))
+
+(define (get-salary name records)
+  ((get 'get-salary (type-tag records)) name records))
+
+(define (find-employee-record name record-files)
+  (if (null? record-files) #f
+    (let ((found-record ((get 'get-record (type-tag (car record-files))) name (car record-files))))
+      (if (equal? found-record #f)
+        (find-employee-record name (cdr record-files))
+        found-record))))
 
 (install-boston-records)
 (install-tampa-records)
 (install-seattle-records)
 
-(define records (get 'records 'boston))
+(define boston-records (get 'records 'boston))
+(define tampa-records (get 'records 'tampa))
+(define seattle-records (get 'records 'seattle))
 
-(define (get-record name records)
-  (contents ((get 'get-record (type-tag records)) name records)))
+(newline)
+(display (get-record "margaret" boston-records))
+(newline)
+(display (get-salary "margaret" boston-records))
 
-(display (get-record "margaret" records))
+(newline)
+(display (get-record "joe" tampa-records))
+(newline)
+(display (get-salary "joe" tampa-records))
+
+(newline)
+(display (get-record "megan" seattle-records))
+(newline)
+(display (get-salary "megan" seattle-records))
+
+(newline)
+(display (find-employee-record "rhys" record-files))
+
+; a. Individual division files might be quite free form, however the file should be tagged with the department name
+; b. Individual records inside division files need to be tagged with the division name
+; d. When a new division/company is added its records file should be installed and register for itself three objects:
+; 'records', 'get-record' and 'get-salary' and also append its records to the global list of record files
