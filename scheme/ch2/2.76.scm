4f7da9033f2fdea191eafab6fa8628e3bbf19dbe
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-07-23 22:40:46 +0300

    Excercise 2.76

--- scheme/ch2/2.76.scm
+++ scheme/ch2/2.76.scm
@@ -13,6 +13,7 @@
 
 (define (generic-dispatch)
   (define (perimeter-rectangle r) (* 2 (+ (car r) (cdr r))))
+  (define (area-rectangle r) (* (car r) (cdr r)))
   (define (make-rectangle x y)
     (attach-tag 'rectangle
               (cons x y)))
@@ -20,23 +21,51 @@
     (eq? (type-tag r) 'rectangle))
 
   (define (perimeter-circle r) (* 2 pi r))
+  (define (area-circle r) (* pi r r))
   (define (make-circle r)
    (attach-tag 'circle r))
   (define (circle? c)
     (eq? (type-tag c) 'circle))
 
+  (define (perimeter-line l) l)
+  (define (area-line l) 0)
+  (define (make-line l)
+    (attach-tag 'line l))
+  (define (line? l)
+    (eq? (type-tag l) 'line))
+
   (define (perimeter z)
     (cond ((rectangle? z) (perimeter-rectangle (contents z)))
           ((circle? z) (perimeter-circle (contents z)))
+          ((line? z) (perimeter-line (contents z)))
           (else (error "Unknown type -- PERIMETER" z))))
+
+  (define (area z)
+    (cond ((rectangle? z) (area-rectangle (contents z)))
+          ((circle? z) (area-circle (contents z)))
+          ((line? z) (area-line (contents z)))
+          (else (error "Unknown type -- AREA" z))))
 
   (define rect (make-rectangle 3 4))
   (define circ (make-circle 1))
+  (define line (make-line 5))
 
+  (newline)
+  (display "generic-dispatch")
   (newline)
   (display (perimeter rect))
   (newline)
+  (display (area rect))
+  (newline)
   (display (perimeter circ))
+  (newline)
+  (display (area circ))
+  (newline)
+  (display (perimeter line))
+  (newline)
+  (display (area line))
+  (newline)
+  (display "end")
 'done)
 
 (define (data-directed-dispatch)
@@ -51,32 +80,60 @@
               (list op (type-tag arg))))))
 
   (define (perimeter-rectangle r) (* 2 (+ (car r) (cdr r))))
+  (define (area-rectangle r) (* (car r) (cdr r)))
   (define (make-rectangle x y)
     (attach-tag 'rectangle
               (cons x y)))
   (put 'perimeter 'rectangle perimeter-rectangle)
+  (put 'area 'rectangle area-rectangle)
 
   (define (perimeter-circle r) (* 2 pi r))
+  (define (area-circle r) (* pi r r))
   (define (make-circle r)
    (attach-tag 'circle r))
   (put 'perimeter 'circle perimeter-circle)
+  (put 'area 'circle area-circle)
+
+  (define (perimeter-line l) l)
+  (define (area-line l) 0)
+  (define (make-line l)
+   (attach-tag 'line l))
+  (put 'perimeter 'line perimeter-line)
+  (put 'area 'line area-line)
 
   (define (perimeter z)
     (apply-generic 'perimeter z))
 
+  (define (area z)
+    (apply-generic 'area z))
+
   (define rect (make-rectangle 3 4))
   (define circ (make-circle 1))
+  (define line (make-line 5))
 
+  (newline)
+  (display "data-directed-dispatch")
   (newline)
   (display (perimeter rect))
   (newline)
+  (display (area rect))
+  (newline)
   (display (perimeter circ))
+  (newline)
+  (display (area circ))
+  (newline)
+  (display (perimeter line))
+  (newline)
+  (display (area line))
+  (newline)
+  (display "end")
 'done)
 
 (define (message-passing-dispatch)
   (define (make-rectangle x y)
     (define (dispatch op)
       (cond ((eq? op 'perimeter) (* 2 (+ x y)))
+            ((eq? op 'area) (* x y))
             (else
               (error "Unknown op -- rectangle" op))))
     dispatch)
@@ -84,23 +141,52 @@
   (define (make-circle r)
     (define (dispatch op)
       (cond ((eq? op 'perimeter) (* 2 pi r))
+            ((eq? op 'area) (* pi r r))
             (else
               (error "Unknown op -- circle" op))))
+    dispatch)
+
+  (define (make-line l)
+    (define (dispatch op)
+      (cond ((eq? op 'perimeter) l)
+            ((eq? op 'area) 0)
+            (else
+              (error "Unknown op -- line" op))))
     dispatch)
 
   (define (apply-generic op arg) (arg op))
   (define (perimeter z)
     (apply-generic 'perimeter z))
+  (define (area z)
+    (apply-generic 'area z))
 
   (define rect (make-rectangle 3 4))
   (define circ (make-circle 1))
+  (define line (make-line 5))
 
+  (newline)
+  (display "message-passing-dispatch")
   (newline)
   (display (perimeter rect))
   (newline)
+  (display (area rect))
+  (newline)
   (display (perimeter circ))
+  (newline)
+  (display (area circ))
+  (newline)
+  (display (perimeter line))
+  (newline)
+  (display (area line))
+  (newline)
+  (display "end")
 'done)
 
 (generic-dispatch)
 (data-directed-dispatch)
 (message-passing-dispatch)
+
+; In all the scenarios data-directed style is better than explicit dispatch
+
+; When adding new types fewer changes when using the explicit dispatch or data-directed style than with message-passing style
+; When new operations are added it might be more convenient to use the message passing style
