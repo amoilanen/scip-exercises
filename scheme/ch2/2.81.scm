3ac925d2f6cbe4bb996093d351f9a82aadee3ca1
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-09-16 23:47:54 +0300

    Excercise 2.81

---
+++ scheme/ch2/2.81.scm
@@ -0,0 +1,195 @@
+(define put '())
+(define get '())
+
+; Adapted version from https://stackoverflow.com/a/5499256/1461965
+(define (define-put-get)
+  (define global-entries '())
+
+  (define (make-entry key value) (cons key value))
+  (define (entry-key entry) (car entry))
+  (define (entry-value entry) (cdr entry))
+
+  (define (put-global key1 key2 value)
+    (define (put-local key entries)
+      (cond ((null? entries) (list (make-entry key value)))
+            ((equal? (entry-key (car entries)) key) (cons (make-entry key value) (cdr entries)))
+            (else (cons (car entries) (put-local key (cdr entries))))))
+    (set! global-entries (put-local (cons key1 key2) global-entries)))
+
+  (define (get-global key1 key2)
+    (define (get-local key entries)
+      (cond ((null? entries) '())
+            ((equal? (entry-key (car entries)) key) (entry-value (car entries)))
+            (else (get-local key (cdr entries)))))
+    (let ((value (get-local (cons key1 key2) global-entries)))
+      (if (null? value)
+        #f
+        value)))
+
+  (set! put put-global)
+  (set! get get-global)
+'done)
+
+(define-put-get)
+
+(define (put-coercion type1 type2 coercion)
+  (put type1 type2 coercion))
+
+(define (get-coercion type1 type2)
+  (get type1 type2))
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+(define (type-tag datum)
+  (cond ((pair? datum) (car datum))
+        ((number? datum) 'scheme-number)
+        (else (error "Bad tagged datum -- TYPE-TAG" datum))))
+(define (contents datum)
+  (cond ((pair? datum) (cdr datum))
+        ((number? datum) datum)
+        (error "Bad tagged datum -- CONTENTS" datum)))
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (if (not (eq? type1 type2))
+                    (let ((t1->t2 (get-coercion type1 type2))
+                          (t2->t1 (get-coercion type2 type1)))
+                        (cond
+                          (t1->t2
+                            (apply-generic op (t1->t2 a1) a2))
+                          (t2->t1
+                            (apply-generic op a1 (t2->t1 a2)))
+                        (else (error "No method for these types"
+                                (list op type-tags)))))
+                    (error "No method for these types"
+                                (list op type-tags))))
+              (error "No method for these types"
+                                (list op type-tags)))))))
+
+;
+; Complex numbers
+;
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a) 
+    (cons (* r (cos a)) (* r (sin a))))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular 
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular 
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+'done)
+
+(define (install-complex-package)
+  (install-rectangular-package)
+  ;; imported procedures from rectangular packages
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  ;; internal procedures
+  (define (real-part z) (apply-generic 'real-part z))
+  (define (imag-part z) (apply-generic 'imag-part z))
+  (define (magnitude z) (apply-generic 'magnitude z))
+  (define (angle z) (apply-generic 'angle z))
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+  ;; interface to rest of the system
+  (define (tag z) (attach-tag 'complex z))
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+'done)
+
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (make-complex-from-real-imag x y)
+  ((get 'make-from-real-imag 'complex) x y))
+
+;
+; Scheme numbers
+;
+(define (install-scheme-number-package)
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (+ x y)))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (- x y)))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (* x y)))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (/ x y)))
+  (put 'exp '(scheme-number scheme-number)
+     (lambda (x y) (expt x y))) ; using primitive expt
+'done)
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+(define (exp x y) (apply-generic 'exp x y))
+
+(install-scheme-number-package)
+(install-complex-package)
+
+(define (scheme-number->complex n)
+  (make-complex-from-real-imag (contents n) 0))
+
+(put-coercion 'scheme-number 'complex scheme-number->complex)
+
+; Conversions type -> type are completely unnecessary, however we should handle them properly if somebody puts them in
+(define (scheme-number->scheme-number n) n)
+(define (complex->complex z) z)
+(put-coercion 'scheme-number 'scheme-number
+              scheme-number->scheme-number)
+(put-coercion 'complex 'complex complex->complex)
+(define c1 (make-complex-from-real-imag 1 2))
+
+(newline)
+(display (add c1 3))
+
+; a. We will get into an infinite loop apply-generic trying to call apply-generic if it is not modified as requested in c.
+(newline)
+(display (exp c1 c1))
+
+; b. Nothing has to be done for coercion of arguments of the same type, apply-generic works as it is as all the generic operations are defined for the same types
