73f6e68d843887a3fe35b72b60480f1035941c5d
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-12-03 22:57:33 +0000

    Excercise 2.88

--- scheme/ch2/2.87.88.scm
+++ scheme/ch2/2.87.88.scm
@@ -125,7 +125,7 @@
   (define (adjoin-term term term-list)
     (if (=zero? (coeff term))
       term-list
-      (cons term term-list))) ; TODO: Should we keep the order of the coefficients of the polynomials/sort?
+      (cons term term-list)))
   (define (the-empty-termlist) '())
   (define (first-term term-list) (car term-list))
   (define (rest-terms term-list) (cdr term-list))
@@ -133,7 +133,7 @@
   (define (make-term order coeff) (list order coeff))
   (define (order term) (car term))
   (define (coeff term) (cadr term))
-  (define (add-terms L1 L2)
+  (define (add-terms L1 L2) ; add-terms keeps the property of the term coefficients being sorted
     (cond ((empty-termlist? L1) L2)
           ((empty-termlist? L2) L1)
           (else
@@ -153,9 +153,9 @@
   (define (mul-terms L1 L2)
     (if (empty-termlist? L1)
       (the-empty-termlist)
-      (add-terms (mul-term-by-all-terms (first-term L1) L2)
-                 (mul-terms (rest-terms L1) L2))))
-  (define (mul-term-by-all-terms t1 L)
+      (add-terms (mul-term-by-all-terms (first-term L1) L2) ; this part of 'mul-terms' keeps the property of the term coefficients being sorted
+                 (mul-terms (rest-terms L1) L2)))) ; also keeps the coefficients sorted (recursive property)
+  (define (mul-term-by-all-terms t1 L) ; mul-term-by-all-terms keeps the property of the term coefficients being sorted
     (if (empty-termlist? L)
       (the-empty-termlist)
       (let ((t2 (first-term L)))
@@ -163,6 +163,13 @@
          (make-term (+ (order t1) (order t2))
                     (mul (coeff t1) (coeff t2)))
          (mul-term-by-all-terms t1 (rest-terms L))))))
+  (define (negate-terms terms)
+    (map
+      (lambda (term)
+        (make-term
+          (order term)
+          (- (coeff term))))
+      terms))
   (define (variable? x) (symbol? x))
   (define (same-variable? v1 v2)
     (and (variable? v1) (variable? v2) (eq? v1 v2)))
@@ -173,6 +180,10 @@
                             (term-list p2)))
       (error "Polys not in same var -- ADD-POLY"
              (list p1 p2))))
+  (define (negate-poly p)
+    (make-poly (variable p) (negate-terms (term-list p))))
+  (define (sub-poly p1 p2)
+    (add-poly p1 (negate-poly p2)))
   (define (mul-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
       (make-poly (variable p1)
@@ -185,18 +196,16 @@
   (define (variable p) (car p))
   (define (term-list p) (cdr p))
   (define (zero? p)
-    (define (zero-terms? terms)
-      (if (empty-termlist? terms)
-        #t
-        (let ((t1 (first-term terms)))
-          (if (= 0 (coeff t1))
-            (zero-terms? (rest-terms terms))
-            #f))))
-    (zero-terms? (term-list p)))
+    (every
+      (lambda (term)
+        (= 0 (coeff term)))
+      (term-list p)))
   ;; interface to rest of the system
   (define (tag p) (attach-tag 'polynomial p))
   (put 'add '(polynomial polynomial) 
        (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'sub '(polynomial polynomial) 
+       (lambda (p1 p2) (tag (sub-poly p1 p2))))
   (put 'mul '(polynomial polynomial) 
        (lambda (p1 p2) (tag (mul-poly p1 p2))))
   (put 'make 'polynomial
@@ -239,3 +248,6 @@
 
 (newline)
 (display (=zero? p1)) ; #f
+
+(newline)
+(display (sub p2 p1)) ; (polynomial x (3 1) (2 1) (1 -2))
